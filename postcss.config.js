715a81ef47cd7d5b7264e32f1cca66a2ea023384
Admin User <degendisciple1@gmail.com>
2025-08-14 23:43:22 +0100

    Fix: add Tailwind CSS nesting support

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+  plugins: [
+    require('postcss-nesting'), // MUST come before tailwindcss
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
+}
